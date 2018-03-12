7dde610b1408542e868d2921b0553b4b425aed20
okreng <okrengel46@gmail.com>
2018-03-12 18:03:01 -0400

    Added network to linear_attack_net.py

--- policies/attack/linear_attack_net.py
+++ policies/attack/linear_attack_net.py
@@ -25,7 +25,7 @@ class LinearAttackNet():
 	Class to hold a linear neural network
 	Will be used to learn Attacks in RISK
 	"""
-	def __init__(self,num_territories, is_training=False, model_instance='0', checkpoint_number=-1, verbose=True):
+	def __init__(self, nS, model_instance='0', checkpoint_number=-1, verbose=True):
 		"""
 		Creates a session of the tensorflow graph defined in this module
 		:param num_territories: int required, will throw error if does not agree 
@@ -48,7 +48,52 @@ class LinearAttackNet():
 		self.module_string = 'linear_attack_net'
 		self.action_type_string = 'attack'
 
+		self.nS = nS
+		nA = nS**2
+		self.nA = nA
+
 		restore_path = model_tree(model_instance, checkpoint_number, self.module_string, self.action_type_string, verbose)
 		# print (restore_path)
+
+		features = tf.placeholder(dtype = tf.float32, shape = [None, nS])
+		act = tf.placeholder(dtype = tf.int32)
+
+		# These will come from next state in TD(0) learning
+		labels = tf.placeholder(dtype = tf.float32, shape = [None, nA])
+
+		# Mask with action actually chose
+		loss_weights = tf.placeholder(dtype = tf.float32, shape = [None, nA])
+		
+		# W = tf.Variable(tf.random_uniform([nS,nA], 0, 0.01))
+		# output = tf.matmul(features, W)
+
+		####### Model #######
+		# TODO: CHECK THE MODEL SOMETHING PROBABLY WRONG!
+
+		# Dense Layer
+		dense = tf.layers.dense(inputs = features, units = nS, activation = None, use_bias=True, name = 'dense')
+		# dense1 = tf.layers.dense(inputs = input_layer, units = 32, activation = tf.nn.relu, name = 'dense1', use_bias=True)
+		# dense2 = tf.layers.dense(inputs = dense1, units = 32, activation = tf.nn.relu, name='dense2', use_bias=True)
+		# dense3 = tf.layers.dense(inputs = dense2, units = 32, activation = tf.nn.relu, name='dense3', use_bias=True)
+		# dense4 = tf.layers.dense(inputs=dense3, units=256, activation=tf.nn.relu, name='dense4', use_bias=True)
+
+		# Output Layer
+		output = tf.layers.dense(inputs = dense, units = nA, activation = None, use_bias=True, name = 'output')
+		
+		#####################
+
+		# predict = output[0, act]
+		# predict_ = tf.reshape(predict, [1,1])
+		# labels_ = output
+		# labels_[0, act] = labels
+		loss = tf.losses.mean_squared_error(labels=labels, predictions=output, weights=loss_weights)
+
+		# optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.0001)
+		optimizer = tf.train.AdamOptimizer(learning_rate = 0.0001)
+
+		train_op = optimizer.minimize(loss = loss, global_step = tf.train.get_global_step())
+		
+		# TODO: NOT SURE IF THIS NEEDS TO BE EXPLICITLY SAVED OR NOT
+		self.saver = tf.train.Saver()
 
 		return
