d39e9556cdb4d8893132fa81be018b7ca5b06490
okreng <okrengel46@gmail.com>
2018-03-12 04:44:40 -0400

    Changed policies to classes

--- policies/attack/max_success.py
+++ policies/attack/max_success.py
@@ -37,51 +37,63 @@ import numpy as np
 
 global MAX_ARMIES #max armies per player
 
-def max_success(state_vector):
+class MaxSuccess():
 	"""
-	Function for executing maximum battle success
-	:param state_vector: np-array 1D vector of armies on territory
-	:return action_vector: np-array 1D vector of edges to attack along
+	Class to hold the maximum success policy
 	"""
+	def __init__(self):
+		"""
+		Empty constructor so MaxSuccess can be held as an object
+		:params: none
+		:return : none
+		"""
+		return
 
-	# TODO: Remove this code once global armies number is defined
-	MAX_ARMIES = 100 # For testing
+	def enact_policy(state_vector):
+		"""
+		Function for executing maximum battle success
+		:param state_vector: np-array 1D vector of armies on territory
+		:return action_vector: np-array 1D vector of edges to attack along
+		"""
 
-	# For prioritizing bad actions between player and opponent over player-player actions
-	# Will be the threshold value at which the player will pass the turn
-	army_offset = MAX_ARMIES + 1
+		# TODO: Remove this code once global armies number is defined
+		MAX_ARMIES = 100 # For testing
 
-	# Pass value is prioritized below 0 difference matchups
-	pass_value = army_offset - 1
+		# For prioritizing bad actions between player and opponent over player-player actions
+		# Will be the threshold value at which the player will pass the turn
+		army_offset = MAX_ARMIES + 1
+
+		# Pass value is prioritized below 0 difference matchups
+		pass_value = army_offset - 1
 
 
-	T = len(state_vector)
-	# Leaving edge_matrix in as a visualization
-	# edge_matrix[row, col] = action_vector[row*T + col]
-	# edge_matrix = np.zeros((T,T), dtype=int)
-	action_vector = np.zeros((T**2 + 1), dtype=int)
+		T = len(state_vector)
+		# Leaving edge_matrix in as a visualization
+		# edge_matrix[row, col] = action_vector[row*T + col]
+		# edge_matrix = np.zeros((T,T), dtype=int)
+		action_vector = np.zeros((T**2 + 1), dtype=int)
 
-	for terr_row in range(T):
-		for terr_col in range(T):
-			terr_row_armies = state_vector[terr_row]
-			terr_col_armies = state_vector[terr_col]
-			if np.sign(terr_row_armies) == np.sign(terr_col_armies): # Can't attack yourself!
-				# edge_matrix[terr_row, terr_col] = 0
-				action_vector[terr_row*T + terr_col] = 0
-			elif terr_row_armies < 0: # Opponent territories
-				# edge_matrix[terr_row, terr_col] = 0
-				action_vector[terr_row*T + terr_col] = 0
-			else:
-				army_difference = abs(terr_row_armies) - abs(terr_col_armies)
-				if army_difference < 0: # If worse than passing, decrement so less than pass_value
+		for terr_row in range(T):
+			for terr_col in range(T):
+				terr_row_armies = state_vector[terr_row]
+				terr_col_armies = state_vector[terr_col]
+				if np.sign(terr_row_armies) == np.sign(terr_col_armies): # Can't attack yourself!
 					# edge_matrix[terr_row, terr_col] = 0
-					action_vector[terr_row*T + terr_col] = army_difference + army_offset -1
+					action_vector[terr_row*T + terr_col] = 0
+				elif terr_row_armies < 0: # Opponent territories
+					# edge_matrix[terr_row, terr_col] = 0
+					action_vector[terr_row*T + terr_col] = 0
 				else:
-					# edge_matrix[terr_row, terr_col] = army_difference + army_offset
-					action_vector[terr_row*T + terr_col] = army_difference + army_offset
+					army_difference = abs(terr_row_armies) - abs(terr_col_armies)
+					if army_difference < 0: # If worse than passing, decrement so less than pass_value
+						# edge_matrix[terr_row, terr_col] = 0
+						action_vector[terr_row*T + terr_col] = army_difference + army_offset -1
+					else:
+						# edge_matrix[terr_row, terr_col] = army_difference + army_offset
+						action_vector[terr_row*T + terr_col] = army_difference + army_offset
 
-	action_vector[-1] = pass_value
+		action_vector[-1] = pass_value
 
-	return action_vector
+		return action_vector
 
-	
+		
