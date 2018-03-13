706bceb7f4e129f6f848c5d4f746bcd619ea1703
okreng <okrengel46@gmail.com>
2018-03-13 15:14:59 -0400

    Preparing to write attack function

--- attack_train_test.py
+++ attack_train_test.py
@@ -18,6 +18,11 @@ def parse_arguments():
 	return parser.parse_args()
 
 def main(args):
+	"""
+	Function to train the simplest type of attack network
+	:param args: string command line arguments (none currently)
+	:return : none
+	"""
 
 	args = parse_arguments()
 
@@ -38,25 +43,54 @@ def main(args):
 
 	game_state = np.random.random_integers(1,12,size=(2))
 	enemy_territory = np.random.random_integers(0,1)
+	agent_territory = abs(1-enemy_territory)
 	game_state[enemy_territory] = -game_state[enemy_territory]
 	game_state = np.reshape(game_state,(1,-1))
 
 	whose_turn = np.random.random_integers(0,1)
 	winner = -1
 
-	print(game_state)
+	# print(game_state)
 
-	while(winner == -1):
+	# Initially set as a reference
+	enemy_game_state = game_state 
+
+	# while(winner == -1):
 
 		# Opponent strategy
 		while whose_turn == 1:
-			action = opponent.call_Q(game_state)
-			if action[1] > action[-1]: 
-				attack(enemy_territory)
+			# Enemy acts the same regardless of real game action or simulated for reward
+			action = opponent.call_Q(enemy_game_state)
+			# Attack action, valid only if enemy has more than 1 army
+			if action[1] > action[-1] and enemy_game_state[enemy_territory] > 1:  # attack action
+				enemy_game_state = attack(enemy_game_state, enemy_territory, agent_territory)
+				if game_state[agent_territory] == 0:
+					winner = 1
+					break
 			else:
 				whose_turn = 0
 
-		# if 
+		# if winner == 1:
+		# 	break
+
+		# while whose_turn == 0:
+		# 	action = agent.call_Q(game_state)
+		# 	if action[1] > action[-1] # attack action
+		# 		next_game_state = attack(game_state, agent_territory, enemy_territory)
+		# 		if next_game_state
+
+	return
+
+def attack(game_state, from_territory, to_territory):
+	"""
+	Function to determine the results of an attack
+	:param game_state: the armies in each territory
+	:param from_territory: the index of the territory attacking
+	:param to_territory: the index of the territory defending
+	"""
+
+
+	return new_game_state
 
 
 
