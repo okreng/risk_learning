6da5defd7e813c92413ee0da19a1255313a5ad38
okreng <okrengel46@gmail.com>
2018-04-10 16:41:18 -0400

    4000 epochs of training for 0-19-1

--- risk_game.py
+++ risk_game.py
@@ -621,9 +621,9 @@ class Player():
         :return : none
         """
         if (self.isAgent):
-            print("Player {}, {}, is an agent".format(self.player_id, self.name)) ## following {} policy".format(self.player_id, self.name, self.policy))
+            print("Player {}: {}".format(self.player_id, self.name)) ## following {} policy".format(self.player_id, self.name, self.policy))
         else:
-            print("Player {}, {}, is a bot".format(self.player_id, self.name)) ## following {} policy".format(self.player_id, self.name, self.policy))
+            print("Player {}: {}".format(self.player_id, self.name)) ## following {} policy".format(self.player_id, self.name, self.policy))
 
     def add_armies(self, num_armies):
         """
